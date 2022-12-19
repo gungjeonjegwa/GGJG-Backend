2785fc3670e6b739f6cd646159a9740fcac3dd11
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-19 10:54:35 +0900

    :tada: :: 아이디어 페스티벌 궁전제과 프로젝트

---
+++ src/test/kotlin/com/example/gungjeonjegwa/GungjeonjegwaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.gungjeonjegwa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GungjeonjegwaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
