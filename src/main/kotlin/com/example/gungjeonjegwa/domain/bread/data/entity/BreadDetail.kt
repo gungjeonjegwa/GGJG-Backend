e315c4a400b6d22ed1357925e28a62e910de5509
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-20 14:31:19 +0900

    :sparkles: :: BreadDetail Entity 정의

---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/bread/data/entity/BreadDetail.kt
@@ -0,0 +1,32 @@
+package com.example.gungjeonjegwa.domain.bread.data.entity
+
+import javax.persistence.*
+
+@Entity
+class BreadDetail(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    val content: String,
+
+    val size: Long,
+
+    val stroage: String, // 보관방법
+
+    val expirationDate: String, // 유통기한
+
+    val previewUrl: String, // 미리보기 이미지
+
+    val precaution: String, // 주의사항
+
+    val deliveryNotice: String, // 배송사항 당일배송불가 사항 작성
+
+    val allergy: String, // 알레르기
+
+    val ingredient: String, // 성분표시
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bread_id")
+    val bread: Bread,
+}
