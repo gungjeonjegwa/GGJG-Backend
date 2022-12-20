57df8c2aa64c8436702428f0e10f1bd0add2f237
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-20 12:20:15 +0900

    :see_no_evil: :: application.yml 추가

--- src/main/kotlin/com/example/gungjeonjegwa/GungjeonjegwaApplication.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/GungjeonjegwaApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class GungjeonjegwaApplication
-
 fun main(args: Array<String>) {
+
     runApplication<GungjeonjegwaApplication>(*args)
 }
