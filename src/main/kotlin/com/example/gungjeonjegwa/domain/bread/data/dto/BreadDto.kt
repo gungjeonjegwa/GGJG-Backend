e51fe9e3aeca68bc507e6b215870a651d9645bc3
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-20 22:09:35 +0900

    :sparkles: :: BreadDto

---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/bread/data/dto/BreadDto.kt
@@ -0,0 +1,25 @@
+package com.example.gungjeonjegwa.domain.bread.data.dto
+
+import com.example.gungjeonjegwa.domain.bread.data.enum.Category
+import com.example.gungjeonjegwa.domain.delivery.data.entity.SellDeliveryType
+
+class BreadDto(
+    val id: Long,
+
+    val title: String,
+
+    val price: Long,
+
+    val category: Category,
+
+    val count: Long,
+
+    val isSoldOut: Boolean,
+
+    val previewUrl: String,
+
+    val sellDeliveryType: MutableList<SellDeliveryType>,
+
+) {
+
+}
