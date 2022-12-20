bbf36aa600384f162ee7708e9979879c5d8dead3
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-20 22:08:53 +0900

    :sparkles: :: 전체 빵 게시글 조회 Controller

---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/bread/controller/BreadController.kt
@@ -0,0 +1,25 @@
+import com.example.gungjeonjegwa.domain.bread.data.dto.BreadDto
+import com.example.gungjeonjegwa.domain.bread.data.dto.BreadQueryDto
+import com.example.gungjeonjegwa.domain.bread.data.enum.Category
+import com.example.gungjeonjegwa.domain.bread.service.BreadService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/bread")
+class BreadController(
+    val breadService: BreadService
+) {
+    @GetMapping
+    fun findAllBread(
+        @RequestParam("page") page: Int,
+        @RequestParam("size") size: Int
+    ): ResponseEntity<BreadQueryDto> =
+        breadService.findAllPost(PageRequest.of(page, size))
+            .let { ResponseEntity.ok(it) }
+
