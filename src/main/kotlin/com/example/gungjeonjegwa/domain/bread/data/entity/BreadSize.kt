cb965e0a96413e1adb52650f93f0b29b21d23e37
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-20 14:32:07 +0900

    :sparkles: :: BreadSize Entity 정의

---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/bread/data/entity/BreadSize.kt
@@ -0,0 +1,24 @@
+package com.example.gungjeonjegwa.domain.bread.data.entity
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+
+@Entity
+class BreadSize(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    val size: String,
+
+    val extramoney: Long,
+
+    @ManyToOne
+    @JoinColumn(name = "detailbread_id")
+    val detailBread: BreadDetail
+) {
+}
