2f30c666297e2efb56487fd9503e3b450ef459d1
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-22 08:46:25 +0900

    :sparkles: :: Return 값에 이미지도 포함 시키는 Logic

--- src/main/kotlin/com/example/gungjeonjegwa/domain/bread/service/impl/BreadServiceImpl.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/bread/service/impl/BreadServiceImpl.kt
@@ -1,13 +1,13 @@
 package com.example.gungjeonjegwa.domain.bread.service.impl
 
-import com.example.gungjeonjegwa.domain.bread.data.dto.BreadDetailDto
 import com.example.gungjeonjegwa.domain.bread.data.dto.BreadDetailQueryDto
 import com.example.gungjeonjegwa.domain.bread.data.dto.BreadQueryDto
+import com.example.gungjeonjegwa.domain.bread.data.entity.BreadDetail
 import com.example.gungjeonjegwa.domain.bread.data.enum.Category
 import com.example.gungjeonjegwa.domain.bread.repository.BreadDetailRepository
+import com.example.gungjeonjegwa.domain.bread.repository.BreadImageRepository
 import com.example.gungjeonjegwa.domain.bread.repository.BreadRepository
 import com.example.gungjeonjegwa.domain.bread.repository.BreadSizeRepository
-import com.example.gungjeonjegwa.domain.bread.repository.SellDeliveryTypeRepository
 import com.example.gungjeonjegwa.domain.bread.service.BreadService
 import com.example.gungjeonjegwa.domain.bread.util.BreadConverter
 import com.example.gungjeonjegwa.domain.bread.util.BreadQueryConverter
@@ -19,10 +19,10 @@ import org.springframework.transaction.annotation.Transactional
 class BreadServiceImpl(
     val breadRepository: BreadRepository,
     val breadDetailRepository: BreadDetailRepository,
-    val sellDeliveryTypeRepository: SellDeliveryTypeRepository,
     val breadSizeRepository: BreadSizeRepository,
     val breadConverter: BreadConverter,
-    val breadQueryConverter: BreadQueryConverter
+    val breadQueryConverter: BreadQueryConverter,
+    val breadImageRepository: BreadImageRepository
 ) : BreadService {
     @Transactional
     override fun findAllPost(pagination: PageRequest): BreadQueryDto {
@@ -30,19 +30,23 @@ class BreadServiceImpl(
             .map { breadConverter.toDto(it) }
         return breadQueryConverter.toQueryDto(findBy.get(), findBy.isLast)
     }
-    @Transactional
     override fun findAllPostByCategory(pagination: PageRequest, category: Category): BreadQueryDto {
-        val findByCategory = breadRepository.findByCategory(category, pagination)
+        val findByCategory = breadRepository.findAllByCategory(category, pagination)
             .map { breadConverter.toDto(it) }
         return breadQueryConverter.toQueryDto(findByCategory.get(), findByCategory.isLast)
     }
 
-    @Transactional
-    override fun findPostByIndex(id: Long): BreadDetailQueryDto =
-        breadDetailRepository.findById(id)
+    @Transactional(readOnly = true)
+    override fun findPostByIndex(id: Long): BreadDetailQueryDto {
+        val breadDetail: BreadDetail
+        val bread = breadDetailRepository.findById(id)
             .orElseThrow { RuntimeException("hi") }
+            .also { breadDetail = it }
             .let { breadConverter.toDto(it) to it }
             .let { breadSizeRepository.findAllByDetailBread(it.second) to it.first }
-            .let { breadQueryConverter.toQueryDto(it.first) to it.second }
-            .let { breadQueryConverter.toQueryDto(it.first, it.second) }
+            .let { breadQueryConverter.toBreadSizeDto(it.first) to it.second }
+        return breadImageRepository.findByBreadDetail(breadDetail)
+            .let { breadQueryConverter.toBreadImageDto(it)}
+            .let { breadQueryConverter.toQueryDto(bread.first, bread.second, it) }
+    }
 }
