542840b4878ae908b84ca0bcee5e7d9af183eb7b
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-21 14:17:23 +0900

    :sparkles: :: 빵 게시물정보 조회 Controller

--- src/main/kotlin/com/example/gungjeonjegwa/domain/bread/controller/BreadController.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/bread/controller/BreadController.kt
@@ -1,13 +1,14 @@
 package com.example.gungjeonjegwa.domain.bread.controller
 
-import com.example.gungjeonjegwa.domain.bread.data.dto.BreadDto
+import com.example.gungjeonjegwa.domain.bread.data.dto.BreadDetailDto
+import com.example.gungjeonjegwa.domain.bread.data.dto.BreadDetailQueryDto
 import com.example.gungjeonjegwa.domain.bread.data.dto.BreadQueryDto
 import com.example.gungjeonjegwa.domain.bread.data.enum.Category
 import com.example.gungjeonjegwa.domain.bread.service.BreadService
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -34,4 +35,8 @@ class BreadController(
         breadService.findAllPostByCategory(PageRequest.of(page,size), category)
             .let { ResponseEntity.ok(it) }
 
+    @GetMapping("/{id}")
+    fun findPostByIndex(@PathVariable("id") breadId: Long): BreadDetailQueryDto {
+        return breadService.findPostByIndex(breadId)
+    }
 }
