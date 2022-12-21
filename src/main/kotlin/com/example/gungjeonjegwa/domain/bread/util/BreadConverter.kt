792f9dcd0507ed6d0b0c33d65d652ebf79616736
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-21 14:15:31 +0900

    :sparkles: :: BreadDetail 을 BreadDetailDto 로 바꾸는 toDto Converter

--- src/main/kotlin/com/example/gungjeonjegwa/domain/bread/util/BreadConverter.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/bread/util/BreadConverter.kt
@@ -1,8 +1,14 @@
 package com.example.gungjeonjegwa.domain.bread.util
 
+import com.example.gungjeonjegwa.domain.bread.data.dto.BreadDetailDto
 import com.example.gungjeonjegwa.domain.bread.data.dto.BreadDto
+import com.example.gungjeonjegwa.domain.bread.data.dto.BreadSizeDto
 import com.example.gungjeonjegwa.domain.bread.data.entity.Bread
+import com.example.gungjeonjegwa.domain.bread.data.entity.BreadDetail
 
 interface BreadConverter {
     fun toDto(entity: Bread): BreadDto
+
+    fun toDto(entity: BreadDetail): BreadDetailDto
+
 }
--- src/main/kotlin/com/example/gungjeonjegwa/domain/bread/util/impl/BreadConverterImpl.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/bread/util/impl/BreadConverterImpl.kt
@@ -1,7 +1,11 @@
 package com.example.gungjeonjegwa.domain.bread.util.impl
 
+import com.example.gungjeonjegwa.domain.bread.data.dto.BreadDetailDto
 import com.example.gungjeonjegwa.domain.bread.data.dto.BreadDto
+import com.example.gungjeonjegwa.domain.bread.data.dto.BreadSizeDto
 import com.example.gungjeonjegwa.domain.bread.data.entity.Bread
+import com.example.gungjeonjegwa.domain.bread.data.entity.BreadDetail
+import com.example.gungjeonjegwa.domain.bread.data.entity.BreadSize
 import com.example.gungjeonjegwa.domain.bread.util.BreadConverter
 import org.springframework.stereotype.Component
 
@@ -16,5 +20,18 @@ class BreadConverterImpl : BreadConverter {
         isSoldOut = entity.isSoldOut,
         previewUrl = entity.previewUrl,
         sellDeliveryType = entity.sellDeliveryType,
+    )
+
+    override fun toDto(entity: BreadDetail): BreadDetailDto = BreadDetailDto(
+        id = entity.id,
+        content = entity.content,
+        size = entity.size,
+        stroage = entity.stroage,
+        expirationDate = entity.expirationDate,
+        previewUrl = entity.previewUrl,
+        precaution = entity.precaution,
+        deliveryNotice = entity.deliveryNotice,
+        allergy = entity.allergy,
+        ingredient = entity.ingredient,
     )
 }
