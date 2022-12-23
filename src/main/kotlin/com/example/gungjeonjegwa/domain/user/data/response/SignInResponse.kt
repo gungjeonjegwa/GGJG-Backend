d56c2d6bb7f66a309337d929d24316b0bc0b749f
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-23 10:56:34 +0900

    :sparkles: :: 로그인 Response

---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/user/data/response/SignInResponse.kt
@@ -0,0 +1,10 @@
+package com.example.gungjeonjegwa.domain.user.data.response
+
+class SignInResponse(
+    val userId: String,
+
+    val accessToken: String,
+
+    val refreshToken: String
+) {
+}
