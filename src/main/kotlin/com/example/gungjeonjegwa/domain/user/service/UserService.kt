6d4622bf9f69839dfb993a6947129778caed9405
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-23 11:06:44 +0900

    :sparkles: :: UserService 메소드 정의

---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/user/service/UserService.kt
@@ -0,0 +1,16 @@
+package com.example.gungjeonjegwa.domain.user.service
+
+import com.example.gungjeonjegwa.domain.user.data.request.SignInRequest
+import com.example.gungjeonjegwa.domain.user.data.request.SignUpRequest
+import com.example.gungjeonjegwa.domain.user.data.response.SignInResponse
+import com.example.gungjeonjegwa.domain.user.data.response.UserTokenResponseDto
+
+interface UserService {
+    fun signUp(request: SignUpRequest): String
+
+    fun signIn(request: SignInRequest): SignInResponse
+
+    fun refreshTokenExecute(refreshToken: String): UserTokenResponseDto
+
+    fun signOut()
+}
