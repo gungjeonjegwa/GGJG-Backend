1ee0ad519eec220866404b6933569094044bc585
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-23 11:48:03 +0900

    :sparkles: :: AccessToken 존재 여부 확인하는 필터

---
+++ src/main/kotlin/com/example/gungjeonjegwa/global/configuration/security/filter/JwtTokenFilter.kt
@@ -0,0 +1,31 @@
+package com.example.gungjeonjegwa.global.configuration.security.filter
+
+import com.example.gungjeonjegwa.global.configuration.security.jwt.TokenProvider
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+
+
+
+
+class JwtTokenFilter(
+    private val jwtTokenProvider: TokenProvider
+): OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token: String? = jwtTokenProvider.resolveToken(request)
+        if(!token.isNullOrBlank()) {
+            val authentication = jwtTokenProvider.authentication(token)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+        filterChain.doFilter(request, response)
+    }
+}
