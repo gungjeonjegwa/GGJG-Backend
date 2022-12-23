c4da233bd8a2d3c4b58cce88fafa9c6dec0feaf9
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-23 10:55:51 +0900

    :sparkles: :: UserDto

---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/user/data/dto/UserDto.kt
@@ -0,0 +1,16 @@
+package com.example.gungjeonjegwa.domain.user.data.dto
+
+class UserDto(
+    val id: String,
+
+    val password: String,
+
+    val name: String,
+
+    val phone: String,
+
+    val email: String,
+
+    val refreshToken: String?
+) {
+}
