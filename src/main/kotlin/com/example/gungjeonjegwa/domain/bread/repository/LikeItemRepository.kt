7ba805fd8408730e522a232dab030313c392832e
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-24 19:07:08 +0900

    :sparkles: :: existByUserAndBread 로 관심상품 찾는 repository

--- src/main/kotlin/com/example/gungjeonjegwa/domain/bread/repository/LikeItemRepository.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/bread/repository/LikeItemRepository.kt
@@ -1,9 +1,12 @@
 package com.example.gungjeonjegwa.domain.bread.repository
 
+import com.example.gungjeonjegwa.domain.bread.data.entity.Bread
 import com.example.gungjeonjegwa.domain.bread.data.entity.LikeItem
 import com.example.gungjeonjegwa.domain.user.data.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface LikeItemRepository : JpaRepository<LikeItem, Long> {
     fun findAllByUser(entity: User): MutableList<LikeItem>
+
+    fun existsByUserAndBread(entity: User, bread: Bread): Boolean
 }
