66710b8391a5e2603918101344c0f422f584eaee
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-24 18:34:59 +0900

    :sparkles: :: LikeItemDto

---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/bread/data/dto/LikeItemDto.kt
@@ -0,0 +1,9 @@
+package com.example.gungjeonjegwa.domain.bread.data.dto
+
+import com.example.gungjeonjegwa.domain.user.data.dto.UserDto
+
+class LikeItemDto(
+    val id: Long,
+    val userId: String
+) {
+}
