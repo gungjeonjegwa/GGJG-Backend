0ee0ef2a48ab7cd8f65990c6441950a9cc7a0968
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-26 01:02:33 +0900

    :sparkles: :: CustomAuthenticationEntryPoint 추가

---
+++ src/main/kotlin/com/example/gungjeonjegwa/global/configuration/security/CustomAuthenticationEntryPoint.kt
@@ -0,0 +1,29 @@
+package com.example.gungjeonjegwa.global.configuration.security
+
+import com.example.gungjeonjegwa.global.exception.ErrorCode
+import com.example.gungjeonjegwa.global.exception.ErrorResponse
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.MediaType
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class CustomAuthenticationEntryPoint(
+    private val objectMapper: ObjectMapper
+) : AuthenticationEntryPoint {
+    override fun commence(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authException: AuthenticationException?
+    ) {
+        val errorCode = ErrorCode.UNAUTHORIZED
+        val responseString = objectMapper.writeValueAsString(ErrorResponse(errorCode))
+        response.characterEncoding = "UTF-8"
+        response.status = errorCode.code
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.writer.write(responseString)
+    }
+}
