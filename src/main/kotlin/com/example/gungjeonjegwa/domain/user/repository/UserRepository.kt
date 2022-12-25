cb7147a541a2613b1b5ecb8e910bc59c8311d3e8
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-25 17:47:24 +0900

    :sparkles: :: existsByEmail 검사 Repository

--- src/main/kotlin/com/example/gungjeonjegwa/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/user/repository/UserRepository.kt
@@ -4,4 +4,5 @@ import com.example.gungjeonjegwa.domain.user.data.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<User, String> {
+    fun existsByEmail(email: String): Boolean
 }
