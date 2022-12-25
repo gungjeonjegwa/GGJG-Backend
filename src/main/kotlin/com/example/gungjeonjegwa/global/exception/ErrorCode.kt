176ce84e463231fb85f115a9965c9f6ea13576b8
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-26 01:03:22 +0900

    :sparkles: :: 빵 세부정보를 찾을 수 없는 ErrorCode 추가

--- src/main/kotlin/com/example/gungjeonjegwa/global/exception/ErrorCode.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/global/exception/ErrorCode.kt
@@ -10,5 +10,5 @@ enum class ErrorCode(
     USER_NOT_FOUND("유저를 찾을 수 없습니다.", 404),
     PASSWORD_NOT_MATCHED("패스워드가 올바르지 않습니다.", 404),
     BREAD_NOT_FOUND("빵 정보를 찾을 수 없습니다.", 404),
-    BREADDETAIL_NOT_FOUND("빵 세부정보를 찾을 수 없습니다.", 404)
+    BREADDETAIL_NOT_FOUND("빵 세부정보를 찾을 수 없습니다.", 404),
 }
