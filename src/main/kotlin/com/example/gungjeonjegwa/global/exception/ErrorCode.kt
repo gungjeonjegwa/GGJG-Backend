c6d4e3b7a4526e99f451e138c46312163ec5e1a2
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-26 01:03:06 +0900

    :sparkles: :: ErrorCode에 권한 없음 추가

--- src/main/kotlin/com/example/gungjeonjegwa/global/exception/ErrorCode.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/global/exception/ErrorCode.kt
@@ -4,6 +4,7 @@ enum class ErrorCode(
     val msg: String,
     val code: Int
 ) {
+    UNAUTHORIZED("권한 없음", 401),
     EXPIRED_TOKEN("토큰 만료", 401),
     INVALID_TOKEN("토큰 변질", 401),
     USER_NOT_FOUND("유저를 찾을 수 없습니다.", 404),
