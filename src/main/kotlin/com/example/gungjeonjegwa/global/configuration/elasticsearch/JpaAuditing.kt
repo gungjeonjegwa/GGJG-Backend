88d1d54f664c0b78df0fe84949e175a6f8efebbf
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-27 10:59:16 +0900

    :sparkles: :: JpaAuditing 추가

---
+++ src/main/kotlin/com/example/gungjeonjegwa/global/configuration/elasticsearch/JpaAuditing.kt
@@ -0,0 +1,9 @@
+package com.example.gungjeonjegwa.global.configuration.elasticsearch
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class JpaAuditing {
+}
