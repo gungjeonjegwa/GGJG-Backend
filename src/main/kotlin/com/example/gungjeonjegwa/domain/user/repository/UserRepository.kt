54b85e533f2a7a2649cb4c8687eb22549aa0cb11
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-27 11:01:49 +0900

    :sparkles: :: /basket/** 인증 url로 변경

--- src/main/kotlin/com/example/gungjeonjegwa/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/user/repository/UserRepository.kt
@@ -2,6 +2,7 @@ package com.example.gungjeonjegwa.domain.user.repository
 
 import com.example.gungjeonjegwa.domain.user.data.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
 interface UserRepository : JpaRepository<User, String> {
     fun existsByEmail(email: String): Boolean
