dba5a9621b915e24bb96ba906ee5ee3da1581ac6
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-27 10:58:13 +0900

    :sparkles: :: @EnableJpaRepositores breadSerivceSearch repository만 사용하게 설정

--- src/main/kotlin/com/example/gungjeonjegwa/GungjeonjegwaApplication.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/GungjeonjegwaApplication.kt
@@ -1,12 +1,21 @@
 package com.example.gungjeonjegwa
 
+import com.example.gungjeonjegwa.domain.bread.repository.BreadSearchRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-@SpringBootApplication
+@EnableJpaRepositories(
+    excludeFilters = [ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = [BreadSearchRepository::class]
+    )]
+)
 @ConfigurationPropertiesScan
+@SpringBootApplication
 class GungjeonjegwaApplication
 fun main(args: Array<String>) {
     runApplication<GungjeonjegwaApplication>(*args)
