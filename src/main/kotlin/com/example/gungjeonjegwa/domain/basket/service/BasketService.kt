fa7f22ca05f8af79521e6501db1ce2367a8ac448
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-27 11:05:30 +0900

    :sparkles: :: findBasketByUser 메소드 구현

---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/basket/service/BasketService.kt
@@ -0,0 +1,6 @@
+package com.example.gungjeonjegwa.domain.basket.service
+
+import com.example.gungjeonjegwa.domain.basket.data.dto.BasketDto
+
+interface BasketService {
+    fun findBasketByUser(): List<BasketDto>
---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/basket/service/BasketServiceImpl.kt
@@ -0,0 +1,21 @@
+package com.example.gungjeonjegwa.domain.basket.service
+
+import com.example.gungjeonjegwa.domain.basket.data.dto.BasketDto
+import com.example.gungjeonjegwa.domain.basket.data.entity.Basket
+import com.example.gungjeonjegwa.domain.basket.repository.BasketRepository
+import com.example.gungjeonjegwa.domain.basket.util.BasketConverter
+import com.example.gungjeonjegwa.global.util.UserUtil
+import org.springframework.stereotype.Service
+
+@Service
+class BasketServiceImpl(
+    private val userUtil: UserUtil,
+    private val basketRepository: BasketRepository,
+    private val basketConverter: BasketConverter
+) : BasketService{
+    override fun findBasketByUser(): List<BasketDto> {
+        val currentUser = userUtil.fetchCurrentUser()
+        val baskets = basketRepository.findByUser(currentUser!!)
+            .map { basketConverter.toDto(it) }
+        return baskets
+    }
