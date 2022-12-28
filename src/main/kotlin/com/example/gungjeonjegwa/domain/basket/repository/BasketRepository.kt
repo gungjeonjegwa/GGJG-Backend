95fca7e3d7380fe3486eae1ee0f096f200541f7f
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-28 11:04:12 +0900

    :sparkles: :: existsByBreadAndUserAndBreadSize Repository

--- src/main/kotlin/com/example/gungjeonjegwa/domain/basket/repository/BasketRepository.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/basket/repository/BasketRepository.kt
@@ -1,6 +1,8 @@
 package com.example.gungjeonjegwa.domain.basket.repository
 
 import com.example.gungjeonjegwa.domain.basket.data.entity.Basket
+import com.example.gungjeonjegwa.domain.bread.data.entity.Bread
+import com.example.gungjeonjegwa.domain.bread.data.entity.BreadSize
 import com.example.gungjeonjegwa.domain.user.data.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
@@ -9,4 +11,6 @@ interface BasketRepository : JpaRepository<Basket, Long> {
     fun findByUser(user: User): List<Basket>
 
     fun findByIdAndUser(id: Long, user: User): Basket
+
+    fun existsByBreadAndUserAndBreadSize(bread: Bread, user: User, breadSize: BreadSize?): Boolean
 }
