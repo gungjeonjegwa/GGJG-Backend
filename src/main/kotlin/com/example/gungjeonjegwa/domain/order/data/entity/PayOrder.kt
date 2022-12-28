063aba52e407efbf4708392298aade0665e0b99e
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-28 22:37:15 +0900

    :sparkles: :: PayOrder Entity

---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/order/data/entity/PayOrder.kt
@@ -0,0 +1,36 @@
+package com.example.gungjeonjegwa.domain.order.data.entity
+
+import com.example.gungjeonjegwa.domain.bread.data.entity.Bread
+import com.example.gungjeonjegwa.domain.bread.data.entity.BreadSize
+import com.example.gungjeonjegwa.domain.order.data.enum.ProductType
+import javax.persistence.*
+
+@Entity
+class PayOrder(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    val count: Long,
+
+    val price: Long,
+
+    @Enumerated(EnumType.STRING)
+    val type: ProductType,
+
+    @Column(nullable = true)
+    val age: Long?,
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    val orders: Orders,
+
+    @ManyToOne
+    @JoinColumn(name = "bread_id")
+    val bread: Bread,
+
+    @ManyToOne
+    @JoinColumn(name = "breadsize_id", nullable = true)
+    val breadSize: BreadSize?
+) {
+}
