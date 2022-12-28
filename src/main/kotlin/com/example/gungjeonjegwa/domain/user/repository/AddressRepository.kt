8bea07ffc248c7b24f8f1a7c94d5a927fa311fdf
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-29 04:22:05 +0900

    :sparkles: :: Address findAllbyUser 메소드 정의

---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/user/repository/AddressRepository.kt
@@ -0,0 +1,9 @@
+package com.example.gungjeonjegwa.domain.user.repository
+
+import com.example.gungjeonjegwa.domain.user.data.entity.Address
+import com.example.gungjeonjegwa.domain.user.data.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface AddressRepository : JpaRepository<Address, Long> {
+    fun findAllByUser(user: User): List<Address>
+}
