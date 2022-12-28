1f125a83992f916929bda8d41e279fbec9617d8f
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-28 22:37:24 +0900

    :sparkles: :: Order Entity

---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/order/data/entity/Orders.kt
@@ -0,0 +1,24 @@
+package com.example.gungjeonjegwa.domain.order.data.entity
+
+import com.example.gungjeonjegwa.domain.order.data.enum.ActivityType
+import com.example.gungjeonjegwa.domain.user.data.entity.User
+import javax.persistence.*
+
+@Entity
+class Orders(
+    @Id
+    val id: String,
+
+    @Enumerated(EnumType.STRING)
+    val activity: ActivityType,
+
+    val deliveryPrice: Long,
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    val user: User,
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "orders")
+    val payOrder: MutableList<PayOrder> = arrayListOf()
+) {
+}
