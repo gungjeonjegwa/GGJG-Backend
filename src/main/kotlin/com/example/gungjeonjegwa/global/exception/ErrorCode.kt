8d897bb377523ae856e0e6d97a4e4ba62f6efaf9
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-29 04:21:50 +0900

    :ambulance: :: 미결제 Exception 위치 변경

--- src/main/kotlin/com/example/gungjeonjegwa/global/exception/ErrorCode.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/global/exception/ErrorCode.kt
@@ -6,6 +6,7 @@ enum class ErrorCode(
 ) {
     LESS_REQUEST_DATA("요구하는 Request 정보가 더 필요합니다.", 400),
     OVER_COUNT("장바구니에서 요구한 빵 개수 카운트 수가 초과하였습니다.", 400),
+    PAYMENT_FAILED("미결제 사용자가 구매 요청을 보냈습니다.", 400),
     UNAUTHORIZED("권한 없음", 401),
     EXPIRED_TOKEN("토큰 만료", 401),
     INVALID_TOKEN("토큰 변질", 401),
@@ -13,7 +14,6 @@ enum class ErrorCode(
     PASSWORD_NOT_MATCHED("패스워드가 올바르지 않습니다.", 404),
     BREAD_NOT_FOUND("빵 정보를 찾을 수 없습니다.", 404),
     BREADDETAIL_NOT_FOUND("빵 세부정보를 찾을 수 없습니다.", 404),
-    PAYMENT_FAILED("미결제 사용자가 구매 요청을 보냈습니다.", 400),
     ADDRESS_NOT_FOUND("주소를 찾을 수 없습니다.", 404),
     BASKET_NOT_FOUND("장바구니에 해당하는 아이템이 존재하지 않습니다.", 404),
     EXIST_NOT_BASKET("장바구니에 해당하는 아이템이 이미 존재합니다.", 409),
