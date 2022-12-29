b89742aeb1f9d122edc1b1ef53d8715dd0193f66
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-29 11:14:40 +0900

    :sparkles: :: 결제할때 주문번호 값 추가로 받기

--- src/main/kotlin/com/example/gungjeonjegwa/domain/order/data/request/CreateOrderBuyRequest.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/order/data/request/CreateOrderBuyRequest.kt
@@ -5,6 +5,8 @@ import com.example.gungjeonjegwa.domain.user.data.dto.AddressDto
 class CreateOrderBuyRequest(
     val isPayment: Boolean = false,
 
+    val orderId: String,
+
     val address: AddressDto,
 
     val list: List<CreateOrderRequest>
