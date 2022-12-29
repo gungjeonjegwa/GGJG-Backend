fc8419047b31a7b3730bc6d1a9fcfca74f75d8a9
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-29 11:12:40 +0900

    :ambulance: :: ExistNotBasket -> ExistBasket 으로변경

--- src/main/kotlin/com/example/gungjeonjegwa/domain/basket/exception/ExistBasketException.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/basket/exception/ExistBasketException.kt
@@ -3,5 +3,5 @@ package com.example.gungjeonjegwa.domain.basket.exception
 import com.example.gungjeonjegwa.global.exception.ErrorCode
 import com.example.gungjeonjegwa.global.exception.exceptions.BasicException
 
-class ExistNotBasketException : BasicException(ErrorCode.EXIST_NOT_BASKET) {
+class ExistBasketException : BasicException(ErrorCode.EXIST_BASKET) {
 }
