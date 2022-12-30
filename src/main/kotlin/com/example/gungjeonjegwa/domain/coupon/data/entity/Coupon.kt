2d173dfdac70da94e997d36ec0c48f1848411d60
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-30 15:01:20 +0900

    :sparkles: :: Coupon Entity

--- src/main/kotlin/com/example/gungjeonjegwa/domain/coupon/data/entity/Coupon.kt
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/coupon/data/entity/Coupon.kt
@@ -1,4 +1,28 @@
 package com.example.gungjeonjegwa.domain.coupon.data.entity
 
-class Coupon {
+import com.example.gungjeonjegwa.domain.coupon.data.enum.DisCountType
+import com.example.gungjeonjegwa.global.entity.BaseTimeEntity
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+import javax.persistence.*
+
+@Entity
+class Coupon(
+    @Id
+    val id: String,
+
+    val name: String,
+
+    @Enumerated(EnumType.STRING)
+    val disCountType: DisCountType,
+
+    val couponPrice: Long,
+
+    val isEnabledCoupon: Boolean,
+
+    val finishDate: LocalDateTime,
+
+    @OneToMany(mappedBy = "coupon", fetch = FetchType.LAZY)
+    val couponType: MutableList<CouponType> = mutableListOf()
+) : BaseTimeEntity() {
 }
