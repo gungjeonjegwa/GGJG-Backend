ab61451de437781817aa647602b643b372bb43ac
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-30 15:01:43 +0900

    :sparkles: :: MyCoupon Entity

---
+++ src/main/kotlin/com/example/gungjeonjegwa/domain/coupon/data/entity/MyCoupon.kt
@@ -0,0 +1,32 @@
+package com.example.gungjeonjegwa.domain.coupon.data.entity
+
+import com.example.gungjeonjegwa.domain.user.data.entity.User
+import com.example.gungjeonjegwa.global.entity.BaseTimeEntity
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+
+@Entity
+class MyCoupon(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    val expiredAt: LocalDateTime,
+
+    val isUsed: Boolean = false,
+
+    @ManyToOne
+    @JoinColumn(name = "coupon_id")
+    val coupon: Coupon,
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    val user: User
+) : BaseTimeEntity() {
+}
